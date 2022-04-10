b02bbaa1314f452a16b75b84e1e9f9dbf87a3b06
kengo78 <fulken35@gmail.com>
2022-04-11 07:00:33 +0900

    todoアプリ完成

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,16 @@
+from secrets import choice
 from django.db import models
 
 # Create your models here.
+CHOICE = (('danger', 'high'),('warning','normal'),('primary','low'))
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices = CHOICE
+    )
+    duedate = models.DateField()
     #Djangoのオフィシャルページ　qiitaなどにmodelのもつフィールドが載ってる
     
     def __str__(self):
